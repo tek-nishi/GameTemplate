d959e45665db70e3d05af9e2c5209cb31b83f9ce
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2015-07-16 10:19:13 +0900

    Randomでのassert処理に、メッセージを追加

--- src/lib/random.cpp
+++ src/lib/random.cpp
@@ -18,13 +18,13 @@ void Random::setSeed(const u_int new_seed) {
   
 // [0, last) を返す
 int Random::operator()(const int last) {
-  assert(last > 0);
+  assert((last > 0) && "The value has to be bigger than 0.");
   return engine_() % last;
 }
 
 // [first, last] を返す
 int Random::operator()(const int first, const int last) {
-  assert(first < last);
+  assert((first <= last) && "The last value has to be same or bigger than first value.");
   return first + operator()(last - first + 1);
 }
 
@@ -36,6 +36,6 @@ float Random::operator()() {
 
 // [first, last] を返す
 float Random::operator()(const float first, const float last) {
-  assert(first < last);
+  assert((first <= last) && "The last value has to be same or bigger than first value.");
   return first + (last - first) * operator()();
 }
